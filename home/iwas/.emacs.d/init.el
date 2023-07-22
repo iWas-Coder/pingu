0caa01e211cd621e41593e4b17c48793c672bf88
iWas-Coder <wasymatieh01@gmail.com>
2023-07-23 01:26:35 +0200

    Added more functionalities to GNU Emacs

--- home/iwas/.emacs.d/init.el
+++ home/iwas/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
+   '(minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
