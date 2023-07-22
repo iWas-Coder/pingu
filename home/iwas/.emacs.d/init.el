bbeee4a57a339647ed75e4da0c391996f6759cc2
iWas-Coder <wasymatieh01@gmail.com>
2023-07-22 22:32:25 +0200

    Improved GNU Emacs config. Also updated `.game-pkg.log` file

--- home/iwas/.emacs.d/init.el
+++ home/iwas/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
+   '(gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
