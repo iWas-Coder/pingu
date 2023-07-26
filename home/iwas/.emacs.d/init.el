5e251384cde9c6fbcf7b3822496779b0797d4519
iWas-Coder <wasymatieh01@gmail.com>
2023-07-26 21:47:50 +0200

    More adjustments to GNU Emacs

--- home/iwas/.emacs.d/init.el
+++ home/iwas/.emacs.d/init.el
@@ -8,10 +8,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
+   '(markdown-mode eshell-prompt-extras eshell-git-prompt eshell-syntax-highlighting vterm minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "JetbrainsMono Nerd Font"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.6))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.5))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.4))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.3))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.2)))))
