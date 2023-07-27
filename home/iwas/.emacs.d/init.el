dcc7c008d80725284fd23bfc0e58ac10fe56795c
iWas-Coder <wasymatieh01@gmail.com>
2023-07-27 15:40:37 +0200

    fix: eshell completion

--- home/iwas/.emacs.d/init.el
+++ home/iwas/.emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(markdown-mode eshell-prompt-extras eshell-git-prompt eshell-syntax-highlighting vterm minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
+   '(corfu markdown-mode eshell-prompt-extras eshell-git-prompt eshell-syntax-highlighting vterm minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
