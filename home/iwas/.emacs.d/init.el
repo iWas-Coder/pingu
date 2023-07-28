f398485643c0ae8697e32e888fbfec641a733321
iWas-Coder <wasymatieh01@gmail.com>
2023-07-28 19:40:56 +0200

    emacs: improved eshell with `eat` terminal emulation and aliases

--- home/iwas/.emacs.d/init.el
+++ home/iwas/.emacs.d/init.el
@@ -2,22 +2,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(corfu markdown-mode eshell-prompt-extras eshell-git-prompt eshell-syntax-highlighting vterm minimap org-bullets toc-org gcmh neotree counsel all-the-icons-ivy-rich which-key use-package projectile doom-themes doom-modeline dashboard)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "JetbrainsMono Nerd Font"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.6))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.5))))
- '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.4))))
- '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.3))))
- '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.2)))))
